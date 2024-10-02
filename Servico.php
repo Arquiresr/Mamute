2c0f33a57e62d115ae11744fdaac939d0ff5cf44
Arquiresr <81758554+Arquiresr@users.noreply.github.com>
2024-10-01 22:51:17 -0300

    Add files via upload

---
+++ Servico.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this template
+-->
+<!DOCTYPE html>
+<html dir="ltr" lang="pt">
+    <head>
+        <title>MAMUTE</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="Css/Main.css" />
+		<script>
+
+		</script>
+    </head>
+	<body>
+	<div class = "menu-bar"><div class="pa"><img src = "Logo2.png" style="width:50px;height:50px;left:2px;"/></div><div class="font2"> Mamute</div> <ul>
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="Servico.php">Serviços</a></li>
+  <li><a href="#Motoa.js">Midia</a></li>
+  <li><a href="#about">Quem somos?</a></li>
+</ul></div>
+
+<div class = "f1">Empresa<br / > Quem somos/></div>
+	 <div class = "Rodape">
+ <div class = "fg"><img src = "Logo2.png" width="190px" height="100px"/> </div>
+<div class = "f1">Empresa<br / > Quem somos?<br /> Por que cine?<br /> Trabalhe  com nosco</div>
+ <div class = "f2">Candidato<br / > Blog<br /> Vagas<br /> Ajúudas</div>
+<div class = "f11">Veiculo de comunicação<br /><img src ="https://cdn-icons-png.flaticon.com/512/1936/1936319.png"width="40px" height="40px"/><img src = "https://cdn-icons-png.flaticon.com/256/124/124010.png" width="40px" height="40px"/>
+</div>
+ </div>
+ <div class = "Roda"><div class="fi">@2024 Mamute</div></div>
+	
+	</body>
+	</html>
