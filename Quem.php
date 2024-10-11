5978cf32f54b7e95fce083a4c3df6bb4741cfee7
Arquiresr <81758554+Arquiresr@users.noreply.github.com>
2024-10-10 22:37:23 -0300

    Add files via upload

---
+++ Quem.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<!--
+Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/html.html to edit this template
+-->
+<!DOCTYPE html>
+<html dir="ltr" lang="pt">
+    <head>
+        <title>MAMUTE</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="Css/Main.css" />
+		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+		<script language="javascript" src="java/Main.js"></script>
+    </head>
+	<body>
+	<style>
+	.imagem2{
+ clip-path: circle(50%);
+  width: 200px;
+  height: 200px;
+  left:0%
+	}
+		.imagem3{
+ clip-path: circle(50%);
+  width: 200px;
+  height: 200px;
+  left:10%
+	}
+	
+	</style>
+	<div class = "menu-bar"><div class="pa"><img src = "Logo2.png" style="width:50px;height:50px;left:2px;"/></div><div class="font2"> Mamute</div> <ul>
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="Servico.php">Serviços</a></li>
+  <li><a href="Midia.php">Midia</a></li>
+  <li><a href="Quem.php">Quem somos?</a></li>
+  
+</ul></div>
+<div class = "Bord1"><font2>Quem Somos: Mamute, Sua Jornada Imersiva
+Nós, da Mamute, somos visionários da tecnologia que transformam ideias em experiências imersivas. Com uma equipe apaixonada por inovação, desenvolvemos aplicativos e sistemas multimídia em primeira pessoa que levam você para além das telas.
+
+Ourochinho e Messi, nossos líderes, unem paixão por tecnologia e empreendedorismo para construir um futuro onde a realidade virtual e aumentada são parte do dia a dia. Com uma visão clara e ousada, impulsionamos a criação de soluções que encantam e transformam.</font2></div>
+<div class = "div-para-imagem-rolante3">
+<font1>Principais fundadores</font1><br>
+<div class = "imagem2"><img src = "https://i.pinimg.com/564x/9a/dc/b4/9adcb490685663f77c61bae9da0c1c8f.jpg" width="190px" height="100px"/></div>
+<div class = "imagem3"> <img src = "https://pt.quizur.com/_image?href=https%3A%2F%2Fimg.quizur.com%2Ff%2Fimg61a3d7b9498415.80142866.jpg%3FlastEdited%3D1638127557&w=600&h=600&f=webp"/></div>
+<div class = "Rodape">
+<div class = "fg"><img src = "Logo2.png" width="190px" height="100px"/> </div>
+<div class = "f1">Empresa<br / > Quem somos?<br /> Por que cine?<br /> Trabalhe  com nosco</div>
+ <div class = "f2">Candidato<br / > Blog<br /> Vagas<br /> Ajúudas</div>
+<div class = "f11">Veiculo de comunicação<br /><img src ="https://cdn-icons-png.flaticon.com/512/1936/1936319.png"width="40px" height="40px"/><img src = "https://cdn-icons-png.flaticon.com/256/124/124010.png" width="40px" height="40px"/>
+</div>
+ </div>
+ <div class = "Roda"><div class="fi"><center>©2024 Mamute </center> </div></div>
+	
+	</body>
+	</html>
