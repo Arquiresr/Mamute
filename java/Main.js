af0912015557eb074d77479a056098e8352c8c50
Arquiresr <81758554+Arquiresr@users.noreply.github.com>
2024-10-15 22:38:48 -0300

    Update Main.js

--- java/Main.js
+++ java/Main.js
@@ -10,3 +10,11 @@ imageAutal = imagemAnterior;
 imagemAnterior = aux	
 	
 }
+ document.addEventListener('DOMContentLoaded', function() {
+    const menuButton = document.querySelector('.hamburger');
+    const menu = document.querySelector('.menu-bar ul');
+
+    menuButton.addEventListener('click', () => {
+      menu.classList.toggle('active');
+    });
+  });
